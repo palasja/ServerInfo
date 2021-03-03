40f7b548808c8cefb061498edd9709c53142b4cd
palasja <palasja@gmail.com>
2021-03-03 20:30:06 +0300

    ServerInfo

---
+++ ServerInfo/Model/Organization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerInfo.Model
+{
+    public class Organization
+    {
+        public String Name { get; set; }
+        public int FuNum{ get; set; }
+        public int KoNum { get; set; }
+    }
+}
